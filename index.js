c80175719192880cf458163d160f6c67ed7b4c8a
Dermot Boyle <dermotboyled@gmail.com>
2022-02-20 19:31:58 +0100

    inital commit for future lambda

---
+++ index.js
@@ -0,0 +1,34 @@
+const puppeteer = require('puppeteer');
+
+
+const regex = /(?:(?:https?|ftp):\/\/)?[\w\/\-?=%.]+\.[\w\/\-&?=%.]+/gm;
+
+(async () => {
+const browser = await puppeteer.launch({
+  headless: false
+});
+
+const page = await browser.newPage();    
+    page.on('response', async response => {
+
+        if (response.request().resourceType() === 'xhr' && response.url().includes("https://twitter.com/i/api/graphql")) {
+           const textResponse = await response.text()
+           const arrayOfData = textResponse.split(',')
+
+           const [ _, url, ...rest] = arrayOfData.filter((item) => typeof item === 'string' && item.includes("video.twimg.com")).flatMap((el) => {
+            const url = el.match(regex)
+                return url
+            })
+
+          console.log(url);
+        }
+    })
+
+    await page.goto('https://twitter.com/shanetodd/status/1493884291838787586',{
+        waitUntil: "networkidle0",
+        timeout: 0,
+    });
+
+  await browser.close();
+    
+})()
